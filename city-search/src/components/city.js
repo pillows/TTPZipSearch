123ace7e31b0a99cc4e05534e37390dfa5dd3497
MarjanGit <mansar16@huntersoe.org>
2019-06-05 10:45:02 -0400

    component

---
+++ city-search/src/components/city.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class City extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    render() {
+        return (<div>
+
+        </div>);
+    }
+}
+
+export default City;
